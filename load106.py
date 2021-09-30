056ab26c8a114c65f5ff6c7f02a0aa154259dd66
iriska-gri <iriska190391@mail.ru>
2021-09-30 15:23:48 +0300

    Обработки трех строк 106

--- load106.py
+++ load106.py
@@ -22,42 +22,61 @@ class load106(Orm):
         name='C:/Users/systemsupport/Desktop/1.csv'
         csv_df = pd.read_csv(name, sep=';', header=None, engine='python', encoding = None)
 
-        # csv_df.loc[csv_df[1] == 'SENT', 2] = ''
+        csv_df1 = csv_df[3]
+        new = csv_df1.str.strip()
+        csv_df = csv_df.drop([3], axis=1)
+        csv_df.insert(loc=3, column=3, value = new)
+        print(list(csv_df))
+        # print(new.loc[new == 'Проверка наличия объекта в АИС'])
         # print(csv_df)
+        # print(csv_df['ll'].loc[csv_df['ll'] == 'Проверка наличия объекта в АИС'])
+
+
+
+
+
+        # print(new.loc[new == 'Проверка наличия объекта в АИС'])
+        # ------------------------------------------------------------------------------------------- Подготовка экселевского файла part1
+        # csv_df1 = csv_df.stack().str.strip().unstack() # Удаляет из выгрузки все лишние пробелы спереди и сзади
+        # print(csv_df[3].loc[csv_df[3] == 'Проверка наличия объекта в АИС '])
+        # csv_df.loc[csv_df[1] == "SENT", 0] *= -1 # Замена history_id на отрицательное, где SENT
+        # -------------------------------------------------------------------------------------------
         # myrows = [0, 1, 3, 4, 5, 6, 8, 9, 11, 15, 16, 18, 21, 22, 23, 24, 26, 27]
         dfrows = csv_df[[0]]
+        
         dfrows.columns = ['history_id']
+        # print(dfrows)
         myrows = {1 : 'actions', 3 : 'task_step_name'}
         myloc = 1
+
         for x in myrows:
             self.obrabrows = csv_df[[x]]
+            # self.obrabrows = self.obrabrows.stack().str.strip().unstack()
             self.obrabrows.columns = [myrows[x]]
             if x == 1:
                 self.obrabrows = self.spravkaZamena('id_actions, actions', 'sprav_actions', myrows[x])
-            if x == 3:    
+            if x == 3:
                 self.obrabrows = self.spravkaZamena('task_step_id, task_step_name', 'sprav_task_step_name', myrows[x])
             dfrows.insert(loc=myloc, column=myrows[x], value = self.obrabrows)
             myloc += 1
-
         dfrows = dfrows.drop_duplicates()
-        # dfrows.to_sql('a_106', con=self.connect(*self.set_connect, self.myBD), if_exists="append", index = False)
-        # for row in dfrows.values:
-            # print(row[0])
-            # if row['actions']=='SENT':
-            #     row['history_id']= f"-{row['history_id']}"
-        # dfrows['col1'] = np.where(dfrows['col1'] == 0, dfrows['col2'], dfrows['col1'])
-        # dfrows = dfrows.loc[dfrows['history_id'] == -127259996]
-        # dfrows =  dfrows.replace([np.nan], 1)
-        # dfrows = dfrows.loc[dfrows['task_step_name'].isin([np.nan])] = 1
-        # dfrows['task_step_name'] = np.where(dfrows['task_step_name'] == np.nan, 1, dfrows['task_step_name'])
 
-        # dfrows['task_step_name'].replace([np.nan], 1, inplace=True)
-        # dfrows = dfrows['task_step_name'] = 127259996
-        # -- ------------------------------------------------------------------------------------------------------ Замена значений в исходном файле
+        # ------------------------------------------------------------------------------------------- Подготовка экселевского файла part2
         # где столбец Y больше 13, в столбце Z присвоить 777
-        dfrows.loc[dfrows['actions'] == 34, 'history_id'] *= -1 
         dfrows.loc[dfrows['actions'] == 34, 'task_step_name'] = 1
+        dfrows.loc[dfrows['actions'] == 63, 'task_step_name'] = 1
+        dfrows.loc[dfrows['actions'] == 16, 'task_step_name'] = 1
+        dfrows.loc[dfrows['actions'] == 34, 'history_id'] *= -1
+        # dfrows = dfrows.loc[dfrows['actions'] == 16]
+        # -------------------------------------------------------------------------------------------
+        dfrows.to_sql('a_106', con=self.connect(*self.set_connect, self.myBD), if_exists="append", index = False)
         print(dfrows)
+
+    # def probeliZamena(self):
+    #     csv_df1 = csv_df[3]
+    #     new = csv_df1.str.strip()
+    #     csv_df.insert(loc=3, column='3', value = new)
+
 
     def spravkaZamena(self, spravRows, spravTable, nameRows):    
         dfspravka = orm.mySQL(orm.Selected(spravRows, spravTable), self.myBD)
